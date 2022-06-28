f28b739563dd38e07926a51255d7c3225edf794a
renuevo <renuevo0110@gmail.com>
2022-06-28 23:12:18 +0900

    :tada: init jpa spring-project

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+
+    val kotlinVersion = "1.6.21"
+    val springVersion = "2.7.1"
+    val dependencyManagerVersion = "1.0.11.RELEASE"
+
+    id("org.springframework.boot") version springVersion
+    id("io.spring.dependency-management") version dependencyManagerVersion
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+}
+
+group = "com.github"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+val querydslVersion = "4.4.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.querydsl:querydsl-jpa:$querydslVersion")
+    kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
+    testImplementation("com.querydsl:querydsl-jpa:$querydslVersion")
+    testAnnotationProcessor("com.querydsl:querydsl-apt:$querydslVersion:jpa")
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/github/renuevo/SpringBootJpaInActionApplication.kt
@@ -0,0 +1,11 @@
+package com.github.renuevo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootJpaInActionApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootJpaInActionApplication>(*args)
+}
